f7bc35953eb7ad4d08d33ba80e127b3459ac9cda
djaus2 <davidjones@sportronics.com.au>
2023-08-27 15:43:30 +1000

    Adding CD Messaging to GPSMap app

--- Azure_IoT_Hub_GPS/iot_configs.h
+++ Azure_IoT_Hub_GPS/iot_configs.h
@@ -1,30 +1,38 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#define IOT_CONFIG_WIFI_SSID "APQLZM"
-#define IOT_CONFIG_WIFI_PASSWORD "silly1371"
+#define IOT_CONFIG_WIFI_SSID "<SSID>"
+#define IOT_CONFIG_WIFI_PASSWORD "<SSIDPWD"
 
 // Azure IoT
-#define IOT_CONFIG_IOTHUB_FQDN "AzIoTHubDj1.azure-devices.net"
-#define IOT_CONFIG_DEVICE_ID "AzTelemHubDevice1"
-#define IOT_CONFIG_DEVICE_KEY "oIciiPuh0w1pfnwcTPc6cjGh+bttT/buzNp6OJCIbxQ="
+#define IOT_CONFIG_IOTHUB_FQDN "<HUB PATH>"
+#define IOT_CONFIG_DEVICE_ID "<DEVICE ID>"
+#define IOT_CONFIG_DEVICE_KEY "<DEVICE KEY>"
 
 
 // Publish 1 message every 2 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS_GAP_START 1000 //Start at 1s
 #define TELEMETRY_FREQUENCY_MILLISECS_GAP_MAX 60000  //Will exp up to 60s
-#define LOOP_DELAY 2000
+// delay at end of loop()
+#define LOOP_DELAY 500
+
+// When CD Message sent, trate as a command
+// Split on this to get command-parameter
 #define PAYLOAD_PARAM_SEP '-'
 
+// If not connected can run without serial port
 #define SerialConnected  true
+
+/////////////////////////////////////////////////////////////////////
 
 void SetTelemetryGap(int gap);
 void GPSsetup();
 
-enum Messages {msgtelemetryStart,msgtelemetryStop,msgtelemetryReset,msgtelemetrySet,msgtelemetryNone};
 
 // Receive CD Messages:
 void receivedCallback(char* topic, byte* payload, unsigned int length);
+enum Messages { msgtelemetryStart, msgtelemetryStop, msgtelemetryReset, msgtelemetrySet, msgtelemetryNone };
+
 
 void onActivateRelayCommand(String cmdName, JsonVariant jsonValue);
 
---
+++ GPSMap/Data/SendCloud2DeviceMsgs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace GPSMap.Data
+{
+    public static class SendCloud2DeviceMsgs
+    {
+        private static readonly IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        private static Microsoft.Azure.Devices.IotHubServiceClient serviceClient;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        static string targetDevice = "{device id}";
+
+        private async static Task SendCloudToDeviceMessageAsync(string msg)
+        {
+            var commandMessage = new
+                Message(Encoding.ASCII.GetBytes(msg));
+            await serviceClient.Messages.SendAsync(targetDevice, commandMessage);
+        }
+
+        public static void Setup()
+        {
+            string connectionString = config.GetValue<string>("HubConnectionString");
+            System.Diagnostics.Debug.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = new IotHubServiceClient(connectionString);
+        }
+
+        public static async Task Invoke(string msg)
+        {
+            System.Diagnostics.Debug.WriteLine("Sending C2D message.");
+            await SendCloudToDeviceMessageAsync(msg);
+            System.Diagnostics.Debug.WriteLine("CD Message Sent");
+
+        }
+    }
+}
+
